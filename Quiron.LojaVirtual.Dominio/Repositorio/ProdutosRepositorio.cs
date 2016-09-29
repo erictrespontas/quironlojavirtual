02cdd296ea01114aea745afbb34215e296cbb220
eric.miranda <eric.miranda@PC-SPI-TI-1471>
2016-09-29 14:23:58 -0300

    Entity

---
+++ Quiron.LojaVirtual.Dominio/Repositorio/ProdutosRepositorio.cs
@@ -0,0 +1,17 @@
+﻿using Quiron.LojaVirtual.Dominio.Entidade;
+using System.Collections.Generic;
+
+namespace Quiron.LojaVirtual.Dominio.Repositorio
+{
+    public class ProdutosRepositorio
+    {
+        private readonly EfDbContext _context = new EfDbContext();
+
+        public IEnumerable<Produto> Produtos
+        {
+            get { return _context.Produtos; }
+        }
+
+
+    }
+}

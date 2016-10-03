c7aa0c25d89dc76474041d919d2583ae8a397d9a
eric.miranda <eric.miranda@PC-SPI-TI-1471>
2016-10-03 16:51:48 -0300

    Aula 8

---
+++ Quiron.LojaVirtual.Web/Models/Paginacao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Quiron.LojaVirtual.Web.Models
+{
+    public class Paginacao
+    {
+        public int ItensTotal { get; set; } //Para obter quantos itens eu tenho no banco
+        public int ItensPorPagina { get; set; } //Quantos itens eu quero por página
+        public int PaginaAtual { get; set; } //Qual a página que está sendo exibida no momento
+        public int TotalPagina //Quantas páginas eu vou ter
+        {
+            get
+            {
+                return (int)Math.Ceiling((decimal)ItensTotal / ItensPorPagina);
+            }
+        }
+    }
+}
